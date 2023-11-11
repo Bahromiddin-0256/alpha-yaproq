a867bee119ca253337d163bc3bbe7135869a11a0
bahromiddin <b.ibragimov@tuit.uz>
2023-11-11 16:10:52 +0500

    initial

--- diagnosis/models.py
+++ diagnosis/models.py
@@ -7,6 +7,7 @@ from diagnosis.processing import process_image
 class Diagnosis(BaseModel):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, null=True, related_name="diagnosis")
     image = models.ImageField(upload_to="diagnosis/")
     result = models.CharField(max_length=100, blank=True, null=True)
 
