6a62ba9b4d1ac3b38a8856e40444efc7573d7031
bahromiddin <b.ibragimov@tuit.uz>
2023-11-11 11:39:24 +0500

    initial

---
+++ bot/urls.py
@@ -0,0 +1,10 @@
+import asyncio
+
+from django.conf import settings
+from django.urls import path
+
+from .views import process_update
+
+urlpatterns = [
+    path("<str:token>/", process_update, name=settings.BOT_WEBHOOK_PATH)
+]
