e7a344ad28c8c37ffd37cb4fdc0a5b7e70deaa2e
bahromiddin <b.ibragimov@tuit.uz>
2023-11-11 12:07:03 +0500

    initial

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source venv/bin/activate
-gunicorn --access-logfile - --workers 3 --bind unix:/home/ponipora/ponipora.sock core.wsgi:application
+gunicorn --access-logfile - --workers 3 --bind unix:/home/alpha-yaproq/yaproq.sock main.wsgi:application
