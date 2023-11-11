aa7633819941f6a40e1df9f18140195922056a94
bahromiddin <b.ibragimov@tuit.uz>
2023-11-11 14:45:02 +0500

    initial

--- diagnosis/models.py
+++ diagnosis/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from common.models import BaseModel
+
+
 # Create your models here.
+
+
+class Diagnosis(BaseModel):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
