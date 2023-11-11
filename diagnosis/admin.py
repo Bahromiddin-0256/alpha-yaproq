c8d8c84e00a6cde52d21289f5087d84b57963123
bahromiddin <b.ibragimov@tuit.uz>
2023-11-11 16:23:09 +0500

    initial

--- diagnosis/admin.py
+++ diagnosis/admin.py
@@ -1,32 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Diagnosis, DiseaseLevel, Disease
+from .models import Diagnosis, Disease, DiseaseLevel
 
 
 @admin.register(Diagnosis)
 class DiagnosisAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description', 'image']
-    search_fields = ['name', 'description']
-    list_filter = ['name', 'description']
+    list_display = ["name", "description", "image"]
+    search_fields = ["name", "description"]
+    list_filter = ["name", "description"]
     list_per_page = 10
-    list_editable = ['description', 'image']
-    readonly_fields = ['image']
+    list_editable = ["description", "image"]
+    readonly_fields = ["image"]
 
 
 @admin.register(Disease)
 class DiseaseAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description']
-    search_fields = ['name', 'description']
-    list_filter = ['name', 'description']
+    list_display = ["name", "description"]
+    search_fields = ["name", "description"]
+    list_filter = ["name", "description"]
     list_per_page = 10
-    list_editable = ['description']
+    list_editable = ["description"]
 
 
 @admin.register(DiseaseLevel)
 class DiseaseLevelAdmin(admin.ModelAdmin):
-    list_display = ['disease', 'level', 'description']
-    search_fields = ['disease', 'level', 'description']
-    list_filter = ['disease', 'level', 'description']
+    list_display = ["disease", "level", "description"]
+    search_fields = ["disease", "level", "description"]
+    list_filter = ["disease", "level", "description"]
     list_per_page = 10
-    list_editable = ['level', 'description']
+    list_editable = ["level", "description"]
