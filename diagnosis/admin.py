97de624d37c0a845118e51bfd1b3f0807174f905
bahromiddin <b.ibragimov@tuit.uz>
2023-11-11 18:43:50 +0500

    initial

--- diagnosis/admin.py
+++ diagnosis/admin.py
@@ -6,7 +6,7 @@ from .models import Diagnosis, Disease, DiseaseLevel
 
 @admin.register(Diagnosis)
 class DiagnosisAdmin(admin.ModelAdmin):
-    list_display = ["name", "description", "image", "created_at", "updated_at"]
+    list_display = ["name", "result", "description", "image", "created_at", "updated_at"]
     search_fields = ["name", "description"]
     list_filter = ["name", "description"]
     list_per_page = 10
