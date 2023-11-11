c740b8050518955f3f0081548ec5294f7bdcd694
bahromiddin <b.ibragimov@tuit.uz>
2023-11-11 16:19:38 +0500

    initial

--- diagnosis/admin.py
+++ diagnosis/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Diagnosis
+from .models import Diagnosis, DiseaseLevel, Disease
 
 
 @admin.register(Diagnosis)
@@ -12,3 +12,21 @@ class DiagnosisAdmin(admin.ModelAdmin):
     list_per_page = 10
     list_editable = ['description', 'image']
     readonly_fields = ['image']
+
+
+@admin.register(Disease)
+class DiseaseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name', 'description']
+    list_filter = ['name', 'description']
+    list_per_page = 10
+    list_editable = ['description']
+
+
+@admin.register(DiseaseLevel)
+class DiseaseLevelAdmin(admin.ModelAdmin):
+    list_display = ['disease', 'level', 'description']
+    search_fields = ['disease', 'level', 'description']
+    list_filter = ['disease', 'level', 'description']
+    list_per_page = 10
+    list_editable = ['level', 'description']
--- diagnosis/models.py
+++ diagnosis/models.py
@@ -18,3 +18,27 @@ class Diagnosis(BaseModel):
         result = process_image(self.image.path)
         self.result = result
         return result
+
+
+class Disease(BaseModel):
+    class DiseaseNameType(models.TextChoices):
+        yellow_rust = "yellow_rust", "Yellow Rust"
+        black_rust = "black_rust", "Black Rust"
+        brown_rust = "brown_rust", "Brown Rust"
+        septoria = "septoria", "Septoria"
+        powdery_mildew = "powdery_mildew", "Powdery Mildew"
+
+    name = models.CharField(max_length=100, choices=DiseaseNameType.choices, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class DiseaseLevel(BaseModel):
+    disease = models.ForeignKey(Disease, on_delete=models.CASCADE, related_name="levels")
+    level = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.level
