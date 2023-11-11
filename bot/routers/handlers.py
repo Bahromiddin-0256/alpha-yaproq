2ce8b8b374e8a0446ca8287f17c8350ea8572107
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2023-11-11 17:40:27 +0500

    save image

--- bot/routers/handlers.py
+++ bot/routers/handlers.py
@@ -1,7 +1,7 @@
 import datetime
 import re
 
-from aiogram import F, Router, enums, types
+from aiogram import F, Router, enums, types, Bot
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from django.utils.translation import gettext_lazy as _
@@ -9,9 +9,13 @@ from django.utils.translation import gettext_lazy as _
 from bot.filters.states import GetData
 from bot.keyboards.keyboards import location_btn, menu_keyboard, plantes_btn
 from users.models import User
+from diagnosis.models import Diagnosis
+from django.conf import settings
+from aiogram.client.session.aiohttp import AiohttpSession
+bot_session_ = AiohttpSession()
+
 
 router_handler = Router()
-
 
 # @router_handler.message(Command("start"))
 @router_handler.message(F.text == "💊 Kasallikni tekshirish")
@@ -20,7 +24,7 @@ async def send_welcome(message: types.Message):
 
 
 @router_handler.message(F.text == "🌾 Bug'doy")
-async def send_welcome(message: types.Message, state=FSMContext):
+async def send_welcome(message: types.Message, state: FSMContext):
     await message.reply("Bug'doyzor manzilini yuboring", reply_markup=location_btn)
     await state.set_state(GetData.location)
 
@@ -44,7 +48,7 @@ async def get_location(message: types.Message, state: FSMContext, user: User):
 
 
 @router_handler.message(GetData.day, F.text)
-async def get_day(message: types.Message, state=FSMContext):
+async def get_day(message: types.Message, state: FSMContext):
     await state.update_data(day=message.text)
     print(re.match("\b\d{1,2}/\d{1,2}/\d{4}\b", message.text))
     await message.answer("Bug'doyni hozirgi rasmini yuboring")
@@ -54,12 +58,37 @@ async def get_day(message: types.Message, state=FSMContext):
 
 
 @router_handler.message(GetData.photo, F.text)
-async def get_location(message: types.Message, state=FSMContext):
+async def get_location(message: types.Message, state: FSMContext):
     await message.answer("Bug'doyni rasmini yuboring")
     await state.set_state(GetData.photo)
     
-@router_handler.message(GetData.photo, F.photo)
-async def get_location(message: types.Message, state=FSMContext):
-    photo = message.photo
-    print(photo)
+# @router_handler.message(GetData.photo, F.photo)
+@router_handler.message( F.photo)
+async def get_location(message: types.Message, state: FSMContext, user: User):
+    
+    bot_ = Bot(settings.BOT_TOKEN, parse_mode="HTML", session=bot_session_)
+
+    file_id = message.photo[-1].file_id
+    
+    # Download the photo
+    photo_path = await bot_.get_file(file_id)
+    photo = await bot_.download_file(photo_path.file_path)
+    
+    # Save the photo locally
+    local_path = f'media/diagnosis/{file_id}.jpg'  # Change the directory and file name as needed
+    with open(local_path, 'wb') as photo_file:
+        photo_file.write(photo.read())
+    await bot_session_.close()
+    
+    diagnosis = Diagnosis()
+    # diagnosis.user = user
+    diagnosis.image = f'diagnosis/{file_id}.jpg'
+    diagnosis.name = "Bug'doy"
+    diagnosis.description = "Bug'doy"
+    diagnosis.predict_disease()
+    await diagnosis.asave()
+    
+    await message.answer("Saqlandi", reply_markup=menu_keyboard)
+    # await state.finish()
+    
     
